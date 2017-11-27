1cf86935445678f292875b8267a3f82ccb99f056
Tabish Raza <tabishraza@gmail.com>
2017-11-27 12:43:01 +0000

    fixed datamapper initialize error

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,7 @@
 ENV["RACK_ENV"] = "development"
 require 'sinatra/base'
-require_relative 'models/picture'
-require_relative 'models/venue'
+# require_relative 'models/picture'
+# require_relative 'models/venue'
 
 
 class Makersbnb < Sinatra::Base
