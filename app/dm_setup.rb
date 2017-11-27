b65da6f69b8270545eadd058eeceee9f64bc42df
cristhiandas <cristhiandf94@gmail.com>
2017-11-27 12:39:00 +0000

    minor changes

--- app/dm_setup.rb
+++ app/dm_setup.rb
@@ -3,8 +3,8 @@ require 'dm-postgres-adapter'
 require_relative 'models/picture'
 require_relative 'models/venue'
 
-require 'models/user'
-require 'models/favorites'
+require_relative 'models/user'
+require_relative 'models/favorites'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/makersbnb_#{ENV['RACK_ENV']}")
 DataMapper.finalize
