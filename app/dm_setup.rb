299ae0eefb88fd176990bbafb9095ceaa1e834d9
Tabish Raza <tabishraza@gmail.com>
2017-11-27 12:23:07 +0000

    file requirement for picture and venue

--- app/dm_setup.rb
+++ app/dm_setup.rb
@@ -1,5 +1,7 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
+require_relative 'models/picture'
+require_relative 'models/venue'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/makersbnb_#{ENV['RACK_ENV']}")
 DataMapper.finalize
