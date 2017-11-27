b7eb5befcaec3afc3fe56202c94c25fc4e1eda50
Tabish Raza <tabishraza@gmail.com>
2017-11-27 12:24:00 +0000

    database setup picture

---
+++ app/models/picture.rb
@@ -0,0 +1,10 @@
+class Picture
+include DataMapper::Resource
+
+  property :id, Serial
+  property :path,   FilePath, :required => true
+  property :md5sum, String,   :length => 32, :default => lambda { |r, p| Digest::MD5.hexdigest(r.path.read) if r.path }
+
+  # belongs_to :venue
+
+end
