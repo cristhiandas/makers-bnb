fd55bca305cf085aec46cbc0c68f3370d49e823a
Tabish Raza <tabishraza@gmail.com>
2017-11-27 15:59:23 +0000

    push user entered data to controller for table entry

--- app/public/src/newVenueInterface.js
+++ app/public/src/newVenueInterface.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
 
+  $('#createVenue').click(function(event){
+    event.preventDefault();
+    var title = $('#title').val();
+    var address = $('#address').val();
+    var price = $('#price').val();
+    var picture = $('#picture').val();
+    postVenue(title, address, price, picture);
+  });
+
+  function postVenue(title, address, price, picture){
+
+    console.log(title);
+    $.post('/venue',{ title: title, address: address, price: price, picture: picture})
+  }
+
 })
