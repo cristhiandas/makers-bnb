1dd8f0c3170b01ad771196d5e1e2783fa300e6be
cristhiandas <cristhiandf94@gmail.com>
2017-11-27 12:13:10 +0000

    favorite and users databases

--- app/dm_setup.rb
+++ app/dm_setup.rb
@@ -1,6 +1,9 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
 
+require 'models/user'
+require 'models/favorites'
+
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/makersbnb_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
