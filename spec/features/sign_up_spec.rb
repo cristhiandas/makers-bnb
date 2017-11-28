9d7969fbc3954693284107a3704f97ce917f12d8
Tabish Raza <tabishraza@gmail.com>
2017-11-28 15:17:15 +0000

    test for sign up with same email

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -10,4 +10,18 @@ feature 'Sign up' do
     expect(page).to have_content('Welcome')
   end
 
+  scenario 'wrong password does not enter user' do
+    expect{ sign_up('fakename', '123@test.com', '12345', '22345') }.to_not change(User, :count)
+  end
+
+  scenario 'sign up creates new user' do
+    expect{ sign_up('fakename', '123@test.com', '12345', '12345') }.to change(User, :count)
+  end
+
+  scenario 'user cannot sign up with same email' do
+    sign_up('fakename', '123@test.com', '12345', '12345')
+    expect{ sign_up('fakename', '123@test.com', '12345', '12345') }.to_not change(User, :count)
+  end
+
+
 end
