8b2e351f31f7ae8772ed3cdccddf8c73cafac85a
Tabish Raza <tabishraza@gmail.com>
2017-11-28 12:36:00 +0000

    encrypting password and matching password to confirmation password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,22 @@
+require 'bcrypt'
 class User
   include DataMapper::Resource
 
   property :id, Serial
   property :name, String
   property :email, Text
-  property :password, Text
+  property :password_digest, Text
 
+  attr_reader :password
+  attr_accessor :password_confirmation
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+
+  validates_confirmation_of :password
   has n, :favorites, through: Resource
   has n, :venues, through: Resource
 end
