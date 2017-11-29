5844f07810d6e6dc2aefa5782a4e8c10b2ca83b9
Tabish Raza <tabishraza@gmail.com>
2017-11-29 14:48:40 +0000

    logic for posting date to controller

--- app/public/src/date.js
+++ app/public/src/date.js
@@ -1,3 +1,21 @@
-$( function() {
-  $( "#datepicker" ).datepicker();
-} );
+// // $(document).ready(function(){
+//
+  $( function() {
+    $( "#startDate" ).datepicker({dateFormat: 'dd-mm-yy'});
+    $( "#endDate" ).datepicker({dateFormat: 'dd-mm-yy'});
+
+  });
+
+  function postDate(startDate, endDate){
+    $.post('/view',{startDate: startDate, endDate: endDate});
+  };
+
+  $( function() {
+    $("#startDate").on("change",function(){
+      var startDate = $(this).val();
+      $("#endDate").on("change",function(){
+        var endDate = $(this).val();
+        postDate(startDate, endDate);
+      });
+    });
+  });
