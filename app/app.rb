047f9e3923197ae37307bb12249fa547bb0cda4b
cristhiandas <cristhiandf94@gmail.com>
2017-11-30 15:46:56 +0000

    dates on bookings removed due bug

--- app/app.rb
+++ app/app.rb
@@ -140,9 +140,9 @@ class Makersbnb < Sinatra::Base
     favorites = Favorite.all
     all_user_with_favorites = favorites.user
       all_user_with_favorites.each do |user_with_fave|
-        if user==user_with_fave
-          fave = user.favorites
-          @favorite_venues = fave.venues
+      if user==user_with_fave
+        fave = user.favorites
+        @favorite_venues = fave.venues
       end
     end
     erb :'favorite/index'
@@ -155,13 +155,14 @@ class Makersbnb < Sinatra::Base
   end
 
   get '/bookings' do
-    @user = User.get(session[:user_id])
-    @reservations = Reservation.all
-    @all_users = @reservations.user, "HERERERERERERERERERE"
+    p @user = User.get(session[:user_id])
+    p @reservations = Reservation.all
+    p @all_users = @reservations.user, "HERERERERERERERERERE"
     @all_users.each do |user_with_reservation|
       if @user == user_with_reservation
-        @resvations_of_the_user = @user.reservations
-        @venues = @resvations_of_the_user.venue
+        p @resvations_of_the_user = @user.reservations
+        p @venues = @resvations_of_the_user.venue
+        
       end
     end
     erb :bookings
